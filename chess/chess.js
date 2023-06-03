810d79f1d12f6c1786a95c22ba526eb0039adf60
venizelou andreas <bl4ckdn4@msn.com>
2023-06-03 15:15:46 +0300

    none

--- chess/chess.js
+++ chess/chess.js
@@ -31,7 +31,7 @@ class Chess extends Window {
         
         this.content.appendChild(this.board);
 
-        this.playerA = "ai";
+        this.playerA = "ui";
         this.playerB = "ai";
 
         this.game = {
